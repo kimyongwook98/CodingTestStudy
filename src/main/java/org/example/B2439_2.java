274855a2d4b84a41d8ca385118cf4a7159bf42fd
Kim, yongwook <kyw4846@gmail.com>
2024-04-01 16:18:38 +0900

    BR + StringBuilder

---
+++ src/main/java/org/example/B2439_2.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class B2439_2 {
+
+    public static void main(String[] args) throws IOException { // BufferedReader + StringBuilder
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        br.close();
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
